53c7cc3bfd219d49a1700f02c62a0c084d58a1cf
Ethan Sletteland <ethan.sletteland@gmail.com>
2021-12-13 11:53:26 -0800

    Fix tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import ButtonComponent from "../stories/button/button.component";
 import {ButtonImplementationComponent} from "./components/button-implementation/button-implementation.component";
+import HeaderComponent from "../stories/header/header.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -13,6 +14,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
         ButtonComponent,
+        HeaderComponent,
         ButtonImplementationComponent
       ],
     }).compileComponents();
